90123768bc77d2419e43648f71536d00604d6c49
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-08-01 03:15:22 +0530

    notification route fixed

--- src/components/UnseenChatToast.tsx
+++ src/components/UnseenChatToast.tsx
@@ -27,7 +27,7 @@ const UnseenChatToast: FC<UnseenChatToastProps> = ({
     )}>
     <a
       onClick={() => toast.dismiss(t.id)}
-      href={`/dashboard/chat/${chatHrefConstructor(sessionId, senderId)}`}
+      href={`/dashboard/chats/${chatHrefConstructor(sessionId, senderId)}`}
       className='flex-1 w-0 p-4'>
       <div className='flex items-start'>
         <div className='flex-shrink-0 pt-0.5'>
